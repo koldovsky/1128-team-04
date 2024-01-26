468ae7b476d446ad2d94f098c4277b2c9880a2be
nebby2105 <154923065+nebby2105@users.noreply.github.com>
2024-01-26 14:44:44 +0000

    added task_3 js

---
+++ CodeWars/task_3.js
@@ -0,0 +1,26 @@
+//https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      let minimum = args[0];
+      for (let i = 0; i <= args.length; i++) {
+        if (args[i] < minimum) minimum = args[i];
+      }
+      return minimum;
+    }
+  }
+  
+  //https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+  function circleCircumference(circle) {
+    return 2 * Math.PI * circle.radius;
+  }
+  
+  //https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+  function giveMeFive(obj) {
+    let result = [];
+    for (let key in obj) {
+      if (key.length === 5) result.push(key);
+      if (obj[key].length === 5) result.push(obj[key]);
+    }
+    return result;
+  }
+  
