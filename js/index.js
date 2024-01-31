cf997c1f7499fa4f3d44dcb35ec8cc72b0422729
someiram <154912770+someiram@users.noreply.github.com>
2024-01-31 12:23:51 +0000

    js added

--- js/index.js
+++ js/index.js
@@ -2,4 +2,6 @@ document.addEventListener('partialsLoaded', () => {
     import('./main-collections.js');
     import('./new-arrivals.js')
     import('./get-your-discount.js')
+    import('./brands.js');
+    import('./blog.js');
 });
